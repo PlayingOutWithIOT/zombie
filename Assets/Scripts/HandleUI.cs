5e9e94bc50cacfe8c2e81cdba0752448f13d7540
Gavin Wood <gavinneilwood@gmail.com>
2018-10-25 13:49:22 +0100

    ...

--- Assets/Scripts/HandleUI.cs
+++ Assets/Scripts/HandleUI.cs
@@ -5,29 +5,38 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class HandleUI : MonoBehaviour {
-    public GameObject m_resetScene, m_playAnim, m_hideUI, m_textPosition, m_textReset;
+    public GameObject m_resetScene, m_playAnim, m_textPosition, m_textReset;
     public Canvas m_canvas;
 
     void Start()
     {
         Button resetButton = m_resetScene.GetComponent<Button>();
         Button playButton = m_playAnim.GetComponent<Button>();
-        Button hideUIButton = m_hideUI.GetComponent<Button>();
-
+       
         resetButton.onClick.AddListener(ResetSceneClick);
-        hideUIButton.onClick.AddListener(HideUIClick);
-        //btn2.onClick.AddListener(delegate { TaskWithParameters("Hello"); });
-    }
-
-    void HideUIClick()
-    {
-        //Output this to console when the Button is clicked
-        Debug.Log("You have clicked the button!");
+       
         m_resetScene.SetActive(false);
-        m_hideUI.SetActive(false);
         m_playAnim.SetActive(false);
         m_textPosition.SetActive(false);
         m_textReset.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.H))
+        {
+            ShowUI();
+        }
+    }
+
+    void ShowUI()
+    {
+        //Output this to console when the Button is clicked
+        Debug.Log("You have clicked the button!");
+        m_resetScene.SetActive(true);
+        m_playAnim.SetActive(true);
+        m_textPosition.SetActive(true);
+        m_textReset.SetActive(true);
     }
 
     void ResetSceneClick()
