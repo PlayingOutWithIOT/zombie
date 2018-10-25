2c76688987b86bf2a8fe5dc68eb55c49fcda1e99
Gavin Wood <gavinneilwood@gmail.com>
2018-10-25 11:50:06 +0100

    ...

---
+++ Assets/Scripts/Serial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class Serial : MonoBehaviour {
+
+    private SerialPort stream = new SerialPort("\\\\.\\COM12", 9600);
+
+    // Use this for initialization
+    void Start () {
+        stream.Open();
+        stream.BaseStream.Flush();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (stream.IsOpen )
+        {
+            string value = stream.ReadLine();
+            Debug.Log("Was line" + value);
+        }
+    }
+}
